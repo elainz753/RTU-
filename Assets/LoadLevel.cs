1535ce206fa42cd0c50851407df72f9aca483bb9
Даниил <rasteryayev03@mail.ru>
2019-11-22 11:06:04 +0300

    Initial commit

---
+++ Assets/LoadLevel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadLevel : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("hand"))
+        {
+            Application.LoadLevel(Application.loadedLevel);
+        }
+    }
+}
---
+++ Assets/move.cs
@@ -0,0 +1,26 @@
+﻿
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    public int speed = 5;
+    public GameObject player;
+    public GameObject ball;
+
+    void Start()
+    {
+    }
+    
+    void Update()
+    {
+      
+        if (Input.GetKey(KeyCode.D))
+        {
+            ball.GetComponent<Rigidbody>().AddForce(player.transform.right * speed);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            ball.GetComponent<Rigidbody>().AddForce(-player.transform.right * speed);
+        }                                           
+    }
+}
